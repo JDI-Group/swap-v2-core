05dbfaf7adf104db85f60e08cddf353a7dec2a94
CodingCas <casweeno2000@gmail.com>
2023-05-04 09:23:56 +0100

    Used minified compiled contracts

--- contracts/test/ERC20.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity =0.5.16;
-
-import '../WannseeERC20.sol';
-
-contract ERC20 is WannseeERC20 {
-    constructor(uint _totalSupply) public {
-        _mint(msg.sender, _totalSupply);
-    }
-}
